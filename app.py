d4e9a9d0566a7148c338a96dea92a28ec5fddc22
Carlo Andaya <2507017+carloandaya@users.noreply.github.com>
2023-06-22 00:12:58 -0700

    Use waitress to serve the app

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, redirect, url_for
 from models import db, Car, app, Maintenance, MaintenanceType
-
+from waitress import serve
 
 @app.route("/")
 def index():
@@ -33,4 +33,5 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
 
-    app.run(debug=True, port=8000, host="127.0.0.1")
+    # app.run(debug=True, port=8000, host="127.0.0.1")
+    serve(app, host='0.0.0.0', port=8080)
